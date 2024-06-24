a60ee81deb5ea32dea6df14eb9e2492125e7ff87
Zacchaeus Ndirangu Mwangi <ndirangu.zmwangi@gmail.com>
2024-06-24 17:50:27 +0300

    feat: Define function pointsForWord

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,14 @@
 // Your code here
+export function pointsForWord(word) {
+  const vowels = new Set(["a", "e", "i", "o", "u"]);
+  const wordArray = word.toLowerCase().split("");
+  let points = 0;
+  for (let i = 0; i < wordArray.length; i++) {
+    if (vowels.has(wordArray[i])) {
+      points += 1;
+    } else {
+      points += 2;
+    }
+  }
+  return points;
+}
